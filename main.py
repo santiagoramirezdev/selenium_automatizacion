8258d0c8a83483d3f1bc698c7a80c68c84480b7e
BlackjetX <santiagoxrp@gmail.com>
2022-06-04 03:17:02 -0500

    final

--- main.py
+++ main.py
@@ -3,21 +3,23 @@ import unittest
 from pyunitreport import HTMLTestRunner
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 
 
+
 def automatizacion(eleccion):
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    driver.get('https://www.instant-gaming.com/es')
+    driver.maximize_window()
     if eleccion == '1':
         correo = input(f"ingrese el correo para el registro\n")
         password = "Xana123456"
         primer_nombre = "Santiago"
         apellido = "Ramirez"
         fecha_de_nacimiento = "20/03/1994"
-        driver.get('https://www.instant-gaming.com/es')
-        driver.maximize_window()
-        #driver.implicitly_wait(15)
         menu = driver.find_element(By.CLASS_NAME, "burger")
         menu.click()
         link_registro = driver.find_element(By.ID, "register-manual")
@@ -45,12 +47,46 @@ def automatizacion(eleccion):
         print("se procede a aceptar")
         btn_aceptar = driver.find_element(By.XPATH, "//button[@class='g-recaptcha button']")
         btn_aceptar.click()
-        time.sleep(10)
+        time.sleep(5)
+    elif eleccion == '2':
+        nombre_completo = "Santiago Ramirez"
+        direccion_facturacion = "calle falsa"
+        tarjeta ="111111111111111"
+        fecha_expiracion ="03/02"
+        primer_juego = driver.find_element(By.ID,"ig-preorders-item-1")
+        primer_juego.click()
+        time.sleep(2)
+        btn_comprar = driver.find_element(By.XPATH,"//div[@class='panel item wide']/div[5]/a[2]")
+        btn_comprar.click()
+        time.sleep(2)
+        campo_nombre_completo = driver.find_element(By.XPATH,"//div[@class='address-form raw']/label[1]/input[@name ='fullname']")
+        campo_nombre_completo.send_keys(nombre_completo)
+        time.sleep(1)
+        campo_direccion_facturacion = driver.find_element(By.XPATH, "//div[@class='address-form raw']/label[2]")
+        campo_direccion_facturacion.send_keys(direccion_facturacion)
+        time.sleep(1)
+        time.sleep(5)
+    elif eleccion == '3':
+        actions = ActionChains(driver).key_down(Keys.LEFT_ALT).send_keys('s')
+        actions.perform()
+        time.sleep(2)
+        campo_sistemas = driver.find_element(By.XPATH,"//span[@class='select2-selection select2-selection--single']")
+        campo_sistemas.click()
+        time.sleep(1)
+        sub_campo_sistemas = driver.find_element(By.XPATH,"//input[@class='select2-search__field']")
+        sub_campo_sistemas.send_keys('pc')
+        time.sleep(1)
+        campo_genero = driver.find_element(By.XPATH,"//span[@class='select2-selection select2-selection--multiple textarea-search']")
+        campo_genero.send_keys('Carreras')
+        time.sleep(1)
+        actions = ActionChains(driver).key_down(Keys.ENTER)
+        actions.perform()
+        time.sleep(5)
 
 if __name__ == '__main__':
     eleccion = input(f"selecciones una opcion para hacer con el programa \n"
                      f"1 --> registro en instant gamming \n"
                      f"2 --> compra de un producto \n"
-                     f"3 --> busqueda ed un producto \n")
+                     f"3 --> busqueda de un producto \n")
 
     automatizacion(eleccion)
